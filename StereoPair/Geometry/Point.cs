05ff7df1c97a6cbea2cd3f08b05d3486d8d04a9f
Umqra <nikitos7991@yandex.ru>
2014-10-25 00:12:39 +0600

    Geometry
    Добавил пустую библиотеку под Geometry

--- StereoPair/Geometry/Point.cs
+++ StereoPair/Geometry/Point.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StereoPair
+namespace Geometry
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			
-		}
-	}
+    public class Point
+    {
+    }
 }
