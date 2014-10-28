9615b0df48de6d37edb3c9b914d1cf3a0509ae7b
mpivko <mpivko@yandex.ru>
2014-10-28 15:53:30 +0500

    Point2D template

---
+++ StereoPair/Geometry/Point2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+	public class Point2D
+	{
+		public readonly double x, y;
+
+		public Point2D(double x_, double y_)
+		{
+			x = x_;
+			y = y_;
+		}
+	}
+}
