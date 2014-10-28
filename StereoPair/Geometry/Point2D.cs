b56eec9861e72775a2ac39b48a4dee3f33bd2d2a
Umqra <nikitos7991@yandex.ru>
2014-10-28 17:34:00 +0500

    Sync all version. NOW!

--- StereoPair/Geometry/Point2D.cs
+++ StereoPair/Geometry/Point2D.cs
@@ -10,10 +10,35 @@ namespace Geometry
 	{
 		public readonly double x, y;
 
+		public Point2D()
+		{
+			x = y = 0;
+		}
 		public Point2D(double x_, double y_)
 		{
 			x = x_;
 			y = y_;
 		}
+
+		public static Point2D operator +(Point2D a, Point2D b)
+		{
+			return new Point2D(a.x + b.x, a.y + b.y);
+		}
+
+		public static Point2D operator -(Point2D a, Point2D b)
+		{
+			return new Point2D(a.x - b.x, a.y - b.y);
+		}
+
+		public static Point2D operator * (Point2D a, double k)
+		{
+			return new Point2D(a.x * k, a.y * k);
+		}
+
+		public static Point2D operator *(double k, Point2D a)
+		{
+			return new Point2D(a.x * k, a.y * k);
+		}
+
 	}
 }
